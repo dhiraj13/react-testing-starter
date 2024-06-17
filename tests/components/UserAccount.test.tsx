f632568ec7b46c355a757be5e2cde8f735a04253
Dhiraj Khapangi <dkhapangi13@outlook.com>
2024-06-17 20:00:01 +0545

    testing user account

---
+++ tests/components/UserAccount.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from "@testing-library/react"
+import UserAccount from "../../src/components/UserAccount"
+import { User } from "../../src/entities"
+
+describe("UserAccount", () => {
+  it("should render edit button if user is admin", () => {
+    const user: User = { id: 1, name: "Dhiraj", isAdmin: true }
+    render(<UserAccount user={user} />)
+
+    const button = screen.getByRole("button")
+    expect(button).toBeInTheDocument()
+    expect(button).toHaveTextContent(/edit/i)
+  })
+  it("should not render edit button if user is not admin", () => {
+    const user: User = { id: 1, name: "Dhiraj" }
+    render(<UserAccount user={user} />)
+
+    const button = screen.queryByRole("button")
+    expect(button).not.toBeInTheDocument()
+  })
+  it("should render user name", () => {
+    const user: User = { id: 1, name: "Dhiraj" }
+    render(<UserAccount user={user} />)
+
+    expect(screen.getByText(user.name)).toBeInTheDocument()
+  })
+})
