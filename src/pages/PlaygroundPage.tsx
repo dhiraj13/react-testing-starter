b147aad10251292f59a85f881eddad55eef91410
Dhiraj Khapangi <dkhapangi13@outlook.com>
2024-06-18 13:20:49 +0545

    testing search box

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,9 +1,7 @@
-import ExpandableText from "../components/ExpandableText"
+import SearchBox from "../components/SearchBox"
 
 const PlaygroundPage = () => {
-  const text =
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure quas ipsum nemo nam, modi ab optio dolor in praesentium odio nostrum veritatis maxime? Nulla doloribus, ipsam adipisci minima delectus quae odit et vero, quam assumenda a? Nisi labore repellat vitae id ex fuga ab quidem dolorum quis quisquam necessitatibus amet ducimus fugit sed perferendis temporibus vel placeat, nulla, nam ipsa, quaerat harum asperiores! Tempore quasi accusantium enim, reiciendis inventore minus. Odio, cum! Pariatur qui voluptatum tenetur distinctio accusamus at sint nostrum praesentium delectus recusandae porro, magni accusantium vero! Expedita ab ratione enim doloremque aliquam, cupiditate tenetur dolores sapiente perferendis rem!"
-  return <ExpandableText text={text} />
+  return <SearchBox onChange={(text) => console.log(text)} />
 }
 
 export default PlaygroundPage
