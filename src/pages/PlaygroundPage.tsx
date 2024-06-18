0e15ce7520ac19dae01801bbcb5858c770b021a2
Dhiraj Khapangi <dkhapangi13@outlook.com>
2024-06-18 12:53:27 +0545

    testing expandable text

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,7 +1,9 @@
-import TermsAndConditions from "../components/TermsAndConditions"
+import ExpandableText from "../components/ExpandableText"
 
 const PlaygroundPage = () => {
-  return <TermsAndConditions />
+  const text =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure quas ipsum nemo nam, modi ab optio dolor in praesentium odio nostrum veritatis maxime? Nulla doloribus, ipsam adipisci minima delectus quae odit et vero, quam assumenda a? Nisi labore repellat vitae id ex fuga ab quidem dolorum quis quisquam necessitatibus amet ducimus fugit sed perferendis temporibus vel placeat, nulla, nam ipsa, quaerat harum asperiores! Tempore quasi accusantium enim, reiciendis inventore minus. Odio, cum! Pariatur qui voluptatum tenetur distinctio accusamus at sint nostrum praesentium delectus recusandae porro, magni accusantium vero! Expedita ab ratione enim doloremque aliquam, cupiditate tenetur dolores sapiente perferendis rem!"
+  return <ExpandableText text={text} />
 }
 
 export default PlaygroundPage
